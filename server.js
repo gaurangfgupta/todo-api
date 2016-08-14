3a7ee2a6326420e9d9f2513a0064e20f9b93d75a
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-08-14 17:34:11 +0530

    Get todos

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var todos = [
+    {
+        id: 1,
+        description: 'Meet Mom for lunch',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Go to market',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Buy vegetables',
+        completed: true
+    }
+];
 
-app.get('/', function (req,res) {
+app.get('/', function (req, res) {
     res.send('Todo API root');
 });
 
-app.listen(PORT, function(){
+app.get('/todos', function (req, res) {
+    res.send(todos);
+    // res.json(todos);
+})
+app.listen(PORT, function () {
     console.log('Listening on PORT: ' + PORT);
 });
