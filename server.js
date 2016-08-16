2a56d2af43a6e190c30a636c82e59535beb17309
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-08-16 12:12:35 +0530

    Code cleanup

--- server.js
+++ server.js
@@ -25,8 +25,24 @@ app.get('/', function (req, res) {
 
 app.get('/todos', function (req, res) {
     res.send(todos);
-    // res.json(todos);
-})
+});
+
+app.get('/todo/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+    todos.forEach(function (todo) {
+        if (todo.id === todoId) {
+            matchedTodo = todo;
+        }
+    });
+
+    if (matchedTodo) {
+        res.send(matchedTodo);
+    } else {
+        res.status(404).send;
+    }
+});
+
 app.listen(PORT, function () {
     console.log('Listening on PORT: ' + PORT);
 });
