d6986d8ec824c755d4d5606c3f336123d6c97059
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-08-16 17:47:57 +0530

    Update with PUT todos

--- server.js
+++ server.js
@@ -43,19 +43,46 @@ app.post('/todos', function (req, res) {
     res.json(body);
 });
 
-app.delete('/todos/:id', function (req,res){
+app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
     var matchedTodo = _.findWhere(todos, { id: todoId });
 
-    if (matchedTodo){
-        todos = _.without(todos,matchedTodo);
+    if (matchedTodo) {
+        todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
     } else {
-        res.status(404).json({"error": "No todo found"});
+        res.status(404).json({ "error": "No todo found" });
     }
 
 });
 
+app.put('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+    requiredProperties = ['description', 'completed'];
+    var body = _.pick(req.body, requiredProperties);
+
+    var validAttributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.length > 0) {
+        body.description = body.description.trim();
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+});
 app.listen(PORT, function () {
     console.log('Listening on PORT: ' + PORT);
 });
