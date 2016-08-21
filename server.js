5ca13b7e5cdaccaac7119f182e50d7e562ac5b21
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-08-21 16:19:34 +0530

    User login

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcryptjs = require('bcryptjs');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -138,9 +139,34 @@ app.post('/users', function (req, res) {
         });
 });
 
+//POST /users/login
+app.post('/users/login', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    if (typeof body.email !== 'string' || typeof body.password !== 'string') {
+        return res.status(400).send();
+    }
+    // res.json(body);
+    var where;
+    db.user.findOne({
+        where: {
+            email: body.email
+        }
+    })
+        .then(function (user) {
+            if (!user || !bcryptjs.compareSync(body.password, user.get('password_hash'))){
+                return res.status(401).send();
+            }
+            res.json(user.toPublicJSON());
+        },
+        function (error) {
+            console.log(error);
+            res.status(500).json(error.toJSON());
+        });
+});
+
 db.sequelize.sync(
-    {force: true}
-    ).then(function () {
+    // { force: true }
+).then(function () {
     app.listen(PORT, function () {
         console.log('Listening on PORT: ' + PORT);
     });
