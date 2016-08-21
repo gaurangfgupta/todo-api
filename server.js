efcbc7ba924d23819c8700f67659d719a6950f40
Gaurang Gupta <gaurangfgupta@gmail.com>
2016-08-21 15:51:02 +0530

    Hash and salt passwords

--- server.js
+++ server.js
@@ -131,14 +131,16 @@ app.post('/users', function (req, res) {
     var body = _.pick(req.body, 'email', 'password');
     db.user.create(body)
         .then(function (user) {
-            res.json(user.toJSON());
+            res.json(user.toPublicJSON());
         },
         function (error) {
             res.status(400).json(error);
         });
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync(
+    {force: true}
+    ).then(function () {
     app.listen(PORT, function () {
         console.log('Listening on PORT: ' + PORT);
     });
